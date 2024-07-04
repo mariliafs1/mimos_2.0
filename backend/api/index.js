9c7758a34fe4213ff407352ef525377105ea0983
Marilia Faria <mariliafs_@hotmail.com>
2024-07-04 11:50:11 -0300

    teste3

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,31 @@
-import app from '../index.js';
+import express from "express";
+import routes from "./routes/routes.js";
+import { connectToDb } from "./database/db.js";
+import cors from 'cors';
+import path from "path";
+import { fileURLToPath } from 'url';
+
+
+connectToDb();
+const app = express();
+const port = 3000;
+app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// app.set('views', path.join(__dirname, 'views'));
+
+const frontendPath = path.join(__dirname, '../frontend')
+
+app.use(express.static(frontendPath)); 
+
+app.use(express.urlencoded());
+
+app.use('/', routes)
+
+app.listen(port, ()=>{
+    console.log(`Servidor escutando na porta: ${port} `);
+});
 
 export default app;
